92c63e76525d82f89d57f09ad8f7fb8d2288d471
zegabr <jjgabrielsp@gmail.com>
2023-08-01 23:29:03 -0300

    feat(go): Add basic http server in Go

--- go/main.go
+++ go/main.go
@@ -1,6 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 func main(){
-    println("opa")
+
+    var router = mux.NewRouter()
+    const port string = ":8000"
+    router.HandleFunc("/", func(response http.ResponseWriter, request *http.Request){
+        fmt.Fprintln(response, "hello from Go")
+    })
+
+    fmt.Println("server listening on port", port)
+    log.Fatalln(http.ListenAndServe(port, router))
 }
 
