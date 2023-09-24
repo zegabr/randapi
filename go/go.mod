f6fdd9d378bae25122b77c0261a8f51ddb2f7d5a
zegabr <jjgabrielsp@gmail.com>
2023-09-24 17:55:38 -0300

    feat: connect go-app with the database

--- go/go.mod
+++ go/go.mod
@@ -3,6 +3,7 @@ module randapi.com
 go 1.21
 
 require (
+	github.com/go-sql-driver/mysql v1.7.1
 	github.com/gorilla/mux v1.8.0
 	github.com/stretchr/testify v1.8.4
 )
--- go/repo/mysql-repo.go
+++ go/repo/mysql-repo.go
@@ -1,16 +1,46 @@
 package repository
 
-import "randapi.com/entity"
+import (
+	"database/sql"
+	"fmt"
+	"log"
 
-type repo struct{}
+	_ "github.com/go-sql-driver/mysql"
+	"randapi.com/entity"
+)
 
-func NewMysqlRepository() Repository {
-	return &repo{}
+type repo struct {
+	db *sql.DB
 }
 
-func (*repo) GetRow() (*entity.Row, error) {
-	return &entity.Row{
-		Language: "go",
-		Phrase:   "hello from go", // TODO: connect to mysql and get prepopulated row for golang
-	}, nil
+func NewMysqlRepository() Repository {
+	db, err := sql.Open("mysql", "user:password@tcp(mysql:3306)/database")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &repo{
+		db: db,
+	}
+}
+
+func (r *repo) GetRow() (*entity.Row, error) {
+	query := fmt.Sprintf("SELECT * FROM `languages` WHERE `language` = 'go'")
+	result, err := r.db.Query(query)
+	if err != nil {
+		log.Println("Error getting row:", err)
+		return nil, err
+	}
+	var row entity.Row
+	if result.Next() {
+		err := result.Scan(&row.Language, &row.Phrase)
+		if err != nil {
+			log.Println("Error scanning table row:", err)
+			return nil, err
+		}
+	}
+	if err = result.Err(); err != nil {
+		log.Println("Error iterating table result:", err)
+		return nil, err
+	}
+	return &row, nil
 }
